8e837e3c7c52ff22b8ea8cec99f4b0230eba6df9
Denis <temp4m@yandex.ru>
2022-01-10 23:19:52 +0300

    other tasks from the course

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,28 +1,49 @@
-print('Hello! My name is Am.')
-print('I was created in 2022.')
-print('Please, remind me your name.')
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
-name = input()
 
-print('What a great name you have, ' + name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
 
-# reading all remainders
-remainder3 = int(input())
-remainder5 = int(input())
-remainder7 = int(input())
 
-age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-print("Your age is " + str(age) + "!")
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-print('Now I will prove to you that I can count to any number you want.')
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
 
-# read a number and count to it here
-number = int(input())
-counter = 0
-while counter <= number:
-    print(str(counter) + "!")
-    counter += 1
-print('Completed, have a nice day!')
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    # write your code here
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+# ...
+end()
---
+++ Topics/Declaring a function/Prevent cheating/main.py
@@ -0,0 +1,6 @@
+import math
+
+# your code here
+
+# don't delete this line, please
+math.factorial(23)
